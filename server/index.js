20a2abc6038f21944a085c52e245a5ce7739b88e
Chloe Chong <chloechong.us@gmail.com>
2018-06-14 14:34:55 -0400

    Fixed duplicate require express

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require('express')
 const path = require('path')
-const express = require('express')
 const volleyball = require('volleyball')
 const bodyParser = require('body-parser')
 const passport = require('passport')
