4c8e993602039fed680a8f5bb76ed630f74ee6ce
Chloe Chong <chloechong.us@gmail.com>
2018-06-14 20:56:00 -0400

    Created router for api for all subrouters

---
+++ server/api/index.js
@@ -0,0 +1,12 @@
+const router = require('express').Router()
+module.exports = router
+
+router.use('/yelp', require('./yelp'))
+router.use('./restaurant', require('./restaurant'))
+router.use('./user', require('./user'))
+
+router.use((req, res, next) => {
+  const error = new Error('Not Found')
+  error.status = 404
+  next(error)
+})
--- server/api/yelp.js
+++ server/api/yelp.js
@@ -5,13 +5,17 @@ module.exports = router
 const client = yelp.client(process.env.YELP_APP_ID)
 
 router.get('/nearby', async (req, res, next) => {
+  const { latitude, longitude } = req.body
   const testData = await client.search({
     term: 'restaurants',
-    latitude: 40.705086,
-    longitude: -74.009151,
+    latitude,
+    longitude,
     radius: 1600,
     open_now: true,
     sort_by: 'rating'
   })
   res.json(testData)
 })
+
+// Fullstack coordinates
+// [-74.009151, 40.705086,]
